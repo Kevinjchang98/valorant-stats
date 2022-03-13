aa413c3a7c6ed0ca1f5d4f85433fdc0cc8e3204e
kevinchang <kevinjchang98@gmail.com>
2022-03-13 15:22:57 -0700

    switch from <img> to <Image>

--- components/PlayerSummary.tsx
+++ components/PlayerSummary.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface Props {
     playerData: any;
     mmrData: any;
@@ -7,7 +9,7 @@ const PlayerSummary = ({ playerData, mmrData }: Props) => {
     return (
         <div>
             <h1>{playerData.name}</h1>
-            <img src={playerData.card.small} alt="player icon" />
+            <Image src={playerData.card.small} alt="player icon" />
             <h2>Account Level: {playerData.account_level}</h2>
             <h2>Rank: {mmrData.currenttierpatched}</h2>
             <h2>Elo: {mmrData.elo}</h2>
